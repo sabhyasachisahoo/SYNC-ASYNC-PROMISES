8bc2bb012e046de026cf15d72c92ccdbdd2a30ad
sabhyasachisahoo <sabhyasachisahoo16@gmail.com>
2025-07-11 01:19:39 +0530

    some Q on promises

--- async class2/script.js
+++ async class2/script.js
@@ -20,10 +20,6 @@ orderFood()
     console.log("🚫 Delivery Failed!");
   });
 
-
-
-
-
 // ✅ 2. Chained Promises: User -> Post -> Comments
 
 // Task:
@@ -33,19 +29,68 @@ orderFood()
 // Chain them together using .then() and log the final output.
 
 function getUser() {
-    return new Promise((res,rej) =>{
-        res({id:1,name:"Raj"});
-    })
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res({ id: 1, name: "Raj" });
+    }, 1000);
+  });
 }
 
 function getPosts(userId) {
-    return new Promise((res, rej) => {
-        res(["Title 1", "Title 2", "Title 3"]);
-    });
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(["Title 1", "Title 2", "Title 3"]);
+    }, 1000);
+  });
 }
 
+function getComments(postId) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(["Great post", "amazing content", "Loved it!"]);
+    }, 1000);
+  });
+}
 
-getUser().then(function(data){
+getUser()
+  .then(function (data) {
     console.log(`data fetched: ${data.name}`);
-    
+    return getPosts(data.id);
+  })
+  .then(function (titles) {
+    console.log(titles);
+    return getComments(titles);
+  })
+  .then(function (comments) {
+    console.log(comments);
+  })
+  .finally(function () {
+    console.log("All operations completed.");
+  });
+
+// ✅ 3. Fake API Delay
+
+// Task:
+// Write a function fakeApiCall() that:
+//  - Accepts a string like "users" or "posts"
+//  - Resolves with some dummy data after a randomm delay (1-3 sec)
+
+function fakeApiCall(endpoint) {
+  const data = {
+    users: ["Raj", "John", "Doe"],
+    posts: ["hey buddy", "let's build this", "cool stuff"],
+  }
+  let delay = Math.random() * 2000 + 1000;
+  return new Promise(function (res, rej) {
+    setTimeout(() => {
+      console.log(data[endpoint])
+    }, delay);
+  });
+}
+
+fakeApiCall("users").then(function(data){
+  console.log(data);
+})
+fakeApiCall("posts").then(function(data){
+  console.log(data);
 })
